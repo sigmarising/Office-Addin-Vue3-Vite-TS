0db0e3b17e54b89c8a2d59c4aae5bf6db6bade85
Yun Zhang <zyun@microsoft.com>
2023-12-20 17:48:43 +0800

    [MOD] add vite static copy to enable web office debug

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,23 @@ import { homedir } from "os";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const _homeDir = homedir();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "manifest.xml",
+          dest: "",
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
