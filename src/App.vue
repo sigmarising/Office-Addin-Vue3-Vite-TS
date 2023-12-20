b66895964045792cfd103f66277e7c36bec31ef5
Yun Zhang <zyun@microsoft.com>
2023-12-20 18:37:04 +0800

    [ADD] Add Ability to interact with Excel

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+const onClick = () => {
+  Excel.run(async context => {
+    const range = context.workbook.getSelectedRange();
+    range.format.fill.color = 'green';
+    await context.sync();
+  });
+}
 </script>
 
 <template>
@@ -10,6 +18,8 @@ import TheWelcome from './components/TheWelcome.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
+
+    <button @click="onClick">Interact with Excel</button>
   </header>
 
   <main>
@@ -43,5 +53,33 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
+}
+
+button {
+  border: #34495e 1.5px solid;
+  width: 100%;
+  border-radius: 12px;
+  margin-top: 30px;
+  font-size: 1rem;
+  padding: 3px;
+  transition: all 0.1s ease-in-out;
+}
+
+button:hover {
+  border: #41b883 2px solid;
+  border-radius: 20px;
+  margin-top: 30px;
+  font-size: 1.1rem;
+  color: #41b883;
+  padding: 5px;
+}
+
+button:active {
+  border: #34495e 2px solid;
+  border-radius: 20px;
+  margin-top: 30px;
+  font-size: 1rem;
+  color: #34495e;
+  padding: 4px;
 }
 </style>
