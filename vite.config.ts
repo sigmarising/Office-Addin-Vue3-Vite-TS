2d9c70d8c8181a0c34a528f7df2e57824d859832
Yun Zhang <zyun@microsoft.com>
2023-12-20 18:15:38 +0800

    [MOD] mod vite config to enable web office debug

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
+// Configuration
+const isDev = true;
+const devPort = 8989;
+const prodUrl = "https://your.addin.production.url/";
+
 const _homeDir = homedir();
+const _urlDev = `https://localhost:${devPort}/`;
+const _urlProd = `${prodUrl}`;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +25,15 @@ export default defineConfig({
         {
           src: "manifest.xml",
           dest: "",
+          transform(content) {
+            if (isDev) {
+              return content;
+            } else {
+              return content
+                .toString()
+                .replace(new RegExp(_urlDev, "g"), _urlProd);
+            }
+          },
         },
       ],
     }),
@@ -28,7 +44,8 @@ export default defineConfig({
     },
   },
   server: {
-    port: 8989,
+    host: true,
+    port: devPort,
     https: {
       key: fs.readFileSync(
         path.resolve(`${_homeDir}/.office-addin-dev-certs/localhost.key`)
